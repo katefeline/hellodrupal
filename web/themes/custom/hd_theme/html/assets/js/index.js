a81a277f62e851ff501d11eff21c068987587d2b
dd_diablo <riabchun.vitalii@gmail.com>
2022-04-07 11:10:33 +0300

    Img, library, template.

--- web/themes/custom/hd_theme/html/assets/js/index.js
+++ web/themes/custom/hd_theme/html/assets/js/index.js
@@ -1,1 +1,9 @@
-console.log('test');
+(function ($, Drupal, once) {
+
+  Drupal.behaviors.indexBehavior = {
+    attach: function (context, settings) {
+      //code.
+    }
+  };
+
+})(jQuery, Drupal, once);
