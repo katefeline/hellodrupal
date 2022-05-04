2b1bc9ab9a787a40e48bfe866ca5d5aa0438a472
dd_diablo <riabchun.vitalii@gmail.com>
2022-05-04 18:21:37 +0300

    Slick

--- web/themes/custom/hd_theme/html/assets/js/product.js
+++ web/themes/custom/hd_theme/html/assets/js/product.js
@@ -1,8 +1,15 @@
 (function ($, Drupal, once) {
-  Drupal.behaviors.myModuleBehavior = {
+  Drupal.behaviors.productSliderBehavior = {
     attach: function (context, settings) {
-      $('.slider-for', context).slick({
+      $('.product__images .slider-for .product-slider', context).slick({
       });
+      $('.product__images .slider-nav .product-slider', context).slick({
+        asNavFor: '.product__images .slider-for .product-slider',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        focusOnSelect: true
+      });
+
     }
   };
 })(jQuery, Drupal, once);
