181a92da6df2e025c1ca8200921f4065d6e070c3
dd_diablo <riabchun.vitalii@gmail.com>
2022-05-05 17:45:41 +0300

    Burger menu js.

--- web/themes/custom/hd_theme/html/assets/js/product.js
+++ web/themes/custom/hd_theme/html/assets/js/product.js
@@ -9,7 +9,18 @@
         slidesToScroll: 1,
         focusOnSelect: true
       });
+    }
+  };
 
+  Drupal.behaviors.productTabsBehavior = {
+    attach: function (context, settings) {
+      $(function() {
+        $('.tabs__buttons').on('click', 'div:not(.field-tab__item_active)', function() {
+          $(this).addClass('field-tab__item_active').siblings().removeClass('field-tab__item_active')
+            .closest('div.tabs').find('div.field-content__item').removeClass('field-content__item_active')
+            .eq($(this).index()).addClass('field-content__item_active');
+        })
+      })
     }
   };
 })(jQuery, Drupal, once);
